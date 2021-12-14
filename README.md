fed0981d06d3f34054abe4fd6f15fd5bbc76cf85
igorsimb <fatalhope@gmail.com>
2021-12-14 16:13:35 +0300

    Initial Commit

---
+++ README.md
@@ -0,0 +1,8 @@
+# A simple to-do app
+
+![screnshot](https://github.com/igorsimb/pretty_todo/assets/screenshot.png?raw=true)
+
+## Description: 
+A simple to do app with functionality to add, delete and mark an item as complete. Click on the item to mark it as complete, click again to unmark.
+
+## Deployed at: [igorsimb.pythonanywhere.com](https://igorsimb.pythonanywhere.com/)
---
+++ todo/views.py
@@ -0,0 +1,59 @@
+from django.shortcuts import render, redirect
+from django.views.decorators.http import require_POST
+
+from .models import Todo
+from .forms import TodoForm, NewTodoForm
+
+
+def index(request):
+    todo_list = Todo.objects.order_by('id')
+
+    form = TodoForm()
+    newtodoform = NewTodoForm()
+
+    context = {'todo_list': todo_list, 'form': newtodoform}
+
+    return render(request, 'todo/index.html', context)
+
+
+@require_POST
+def addTodo(request):
+    # form = TodoForm(request.POST)
+    # todo_10 = Todo.objects.get(pk=10)
+    # newtodoform = NewTodoForm(request.POST, instance=todo_10)
+    newtodoform = NewTodoForm(request.POST)
+
+    print(request.POST['text'])
+
+    if newtodoform.is_valid():
+        # new_todo = Todo(text=form.cleaned_data['text'])
+        # new_todo.save()
+        new_todo = newtodoform.save()
+
+    return redirect('index')
+
+
+def completeTodo(request, todo_id):
+    todo = Todo.objects.get(pk=todo_id)
+    todo.complete = True
+    todo.save()
+
+    return redirect('index')
+
+def uncompleteTodo(request, todo_id):
+    todo = Todo.objects.get(pk=todo_id)
+    todo.complete = False
+    todo.save()
+
+    return redirect('index')
+
+
+def deleteCompleted(request):
+    Todo.objects.filter(complete__exact=True).delete()
+
+    return redirect('index')
+
+def deleteAll(request):
+    Todo.objects.all().delete()
+
+    return redirect('index')
